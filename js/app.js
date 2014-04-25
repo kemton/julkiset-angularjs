20dcf757eb9b7d66c1778f78d2efe7314b2736c8
Niko Kiuru <niko@kiuru.net>
2014-04-26 01:10:04 +0300

    Refactoring full project, so in begin generate new project

--- js/app.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var app = angular.module('julkiset', ['ui.bootstrap']).
-	config(['$routeProvider', function($routeProvider) {
-	$routeProvider.
-		when('/home', {templateUrl: 'partials/home.html', controller: HomeController}).
-		when('/route-options/:from/:to/:date/:time/:timetype', {templateUrl: 'partials/route-options.html', controller: RouteController}).
-		otherwise({redirectTo: '/home'});
-}]);
